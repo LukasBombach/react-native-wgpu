b2307a63b8df0a3ae5a96c4756d57643ca22f633
Lukas Bombach <lukas.bombach@googlemail.com>
2025-02-24 11:26:34 +0100

    color -> shader

--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,24 +1,31 @@
 struct VertexInput {
-    @location(0) position: vec3<f32>,
-    @location(1) color: vec3<f32>,
+    @location(0) position: vec2<f32>,
 };
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) color: vec3<f32>,
+    @location(0) color: vec4f,
 };
 
 @vertex
 fn vs_main(
     model: VertexInput,
+    @builtin(vertex_index) vertex_index: u32
 ) -> VertexOutput {
-    var out: VertexOutput;
-    out.color = model.color;
-    out.clip_position = vec4<f32>(model.position, 1.0);
-    return out;
+
+    var color = array<vec4f, 3>(
+        vec4f(1, 0, 0, 1), // red
+        vec4f(0, 1, 0, 1), // green
+        vec4f(0, 0, 1, 1), // blue
+    );
+
+    var output: VertexOutput;
+    output.clip_position = vec4<f32>(model.position, 1.0, 1.0);
+    output.color = color[vertex_index];
+    return output;
 }
 
-@fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(in.color, 1.0);
+@fragment 
+fn fs_main(fsInput: VertexOutput) -> @location(0) vec4f {
+    return fsInput.color;
 }
--- src/wgpu_ctx.rs
+++ src/wgpu_ctx.rs
@@ -20,8 +20,7 @@ pub struct WgpuCtx<'window> {
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
 struct Vertex {
-    position: [f32; 3],
-    color: [f32; 3],
+    position: [f32; 2],
 }
 
 impl Vertex {
@@ -29,46 +28,24 @@ impl Vertex {
         wgpu::VertexBufferLayout {
             array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-            ],
+            attributes: &[wgpu::VertexAttribute {
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float32x2,
+            }],
         }
     }
 }
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
+        position: [0.0, 0.5],
     },
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
+        position: [-0.5, -0.5],
     },
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
-    Vertex {
-        position: [0.2, -0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
-    Vertex {
-        position: [0.7, 0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
-    Vertex {
-        position: [1.2, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
+        position: [0.5, -0.5],
     },
 ];
 
