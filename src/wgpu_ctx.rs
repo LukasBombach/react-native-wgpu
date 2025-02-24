22a765118fd9ac8f7dd4195c3315b3c11577ed29
Lukas Bombach <lukas.bombach@googlemail.com>
2025-02-24 10:58:24 +0100

    two triangles

--- src/wgpu_ctx.rs
+++ src/wgpu_ctx.rs
@@ -47,15 +47,27 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
+        position: [-0.5, -0.5, 0.0],
         color: [1.0, 0.0, 0.0],
     },
     Vertex {
-        position: [-0.5, -0.5, 0.0],
+        position: [0.0, 0.5, 0.0],
         color: [0.0, 1.0, 0.0],
     },
     Vertex {
         position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [0.2, -0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [0.7, 0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [1.2, -0.5, 0.0],
         color: [0.0, 0.0, 1.0],
     },
 ];
