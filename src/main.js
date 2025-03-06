b5f8800fe7b54708e198a1984bec08975a19516f
Lukas Bombach <lukas.bombach@googlemail.com>
2025-03-06 12:11:56 +0100

    js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,12 @@
-const console = {
-  log: value => Deno.core.print(JSON.stringify(value) + "\n"),
-};
+const print = obj => Deno.core.print(JSON.stringify(obj) + "\n");
+const create = Deno.core.ops.op_create_rect;
+const get = Deno.core.ops.op_get_rect;
+const update = Deno.core.ops.op_update_rect;
 
-class Rect {
-  constructor(x, y, width, height) {
-    this.ptr = Deno.core.ops.op_create_rect(x, y, width, height);
-  }
+const ptr = create(100, 100, 100, 100);
 
-  get rect() {
-    return Deno.core.ops.op_get_rect(this.ptr);
-  }
+print(get(ptr));
 
-  update(x, y, width, height) {
-    Deno.core.ops.op_update_rect(this.ptr, x, y, width, height);
-  }
-}
+update(ptr, 100, 100, 600, 600);
 
-const rect = new Rect(100, 100, 100, 100);
-
-console.log(rect.rect);
-rect.update(100, 100, 600, 600);
-console.log(rect.rect);
+print(get(ptr));
