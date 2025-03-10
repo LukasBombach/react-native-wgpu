3e864f8e124fd120ab7ca3c5c9e33c45334ba4b3
Lukas Bombach <lukas.bombach@googlemail.com>
2025-03-10 19:08:31 +0100

    it werks

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const print = (...args) => Deno.core.print(args.join(" ") + "\n");
 
-const rect = new Deno.core.ops.Rect(10, 10, 100, 50);
+const rect = new Deno.core.ops.op_create_rect(100, 100, 200, 200);
 
-Deno.core.ops.op_sync_instance_buffer();
-Deno.core.ops.op_request_redraw();
+//Deno.core.ops.op_sync_instance_buffer();
+//Deno.core.ops.op_request_redraw();
