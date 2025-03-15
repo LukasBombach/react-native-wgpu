181c03bfb3f1c3369bd4e4163bbce0ed0f87fca6
Lukas Bombach <lukas.bombach@googlemail.com>
2025-03-15 23:26:02 +0100

    Using `deno_runtime` instead of `deno_core` (#5)
    * hello world
    
    * totally works

--- src/deno.rs
+++ src/deno.rs
@@ -1,18 +1,26 @@
-use std::env::current_dir;
+#![allow(clippy::print_stdout)]
+#![allow(clippy::print_stderr)]
+
+use std::path::Path;
 use std::rc::Rc;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 
-use deno_core::error::AnyError;
 use deno_core::extension;
 use deno_core::op2;
-use deno_core::resolve_path;
 use deno_core::FsModuleLoader;
-use deno_core::JsRuntime;
+use deno_core::ModuleSpecifier;
 use deno_core::OpState;
 use deno_core::Resource;
-use deno_core::RuntimeOptions;
+use deno_fs::RealFs;
+use deno_resolver::npm::DenoInNpmPackageChecker;
+use deno_resolver::npm::NpmResolver;
+use deno_runtime::deno_permissions::PermissionsContainer;
+use deno_runtime::permissions::RuntimePermissionDescriptorParser;
+use deno_runtime::worker::MainWorker;
+use deno_runtime::worker::WorkerOptions;
+use deno_runtime::worker::WorkerServiceOptions;
 
 use crate::app::AppState;
 use crate::app::Js;
@@ -126,17 +134,21 @@ fn op_remove_rect_from_window(state: &mut OpState, rid: u32) -> Result<(), deno_
 }
 
 extension!(
-    rects,
+    rn_wgpu,
     ops = [
         op_create_rect,
         op_append_rect_to_window,
         op_update_rect,
         op_remove_rect_from_window,
-    ]
+    ],
+    esm_entry_point = "ext:rn_wgpu/rn_wgpu.js",
+    esm = [dir "src", "rn_wgpu.js"]
 );
 
-pub fn run_script(app_state: Arc<Mutex<AppState>>, path: &str) {
-    let path = path.to_string();
+pub fn run_script(app_state: Arc<Mutex<AppState>>, js_path: &str) {
+    // let js_path = js_path.to_string();
+
+    let js_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(js_path);
 
     let _handle = thread::spawn(move || {
         let tokio_runtime = tokio::runtime::Builder::new_current_thread()
@@ -144,24 +156,46 @@ pub fn run_script(app_state: Arc<Mutex<AppState>>, path: &str) {
             .build()
             .unwrap();
 
-        if let Err(error) = tokio_runtime.block_on(async {
-            let mut js_runtime = JsRuntime::new(RuntimeOptions {
-                extensions: vec![rects::init_ops_and_esm()],
-                module_loader: Some(Rc::new(FsModuleLoader)),
-                ..Default::default()
-            });
+        tokio_runtime.block_on(async {
+            let main_module = ModuleSpecifier::from_file_path(js_path).unwrap();
 
-            js_runtime.op_state().borrow_mut().put(app_state);
+            eprintln!("Running {main_module}...");
 
-            let main_module = resolve_path(&path, &current_dir().unwrap()).unwrap();
-            let mod_id = js_runtime.load_main_es_module(&main_module).await?;
-            let result = js_runtime.mod_evaluate(mod_id);
+            let fs = Arc::new(RealFs);
+            let permission_desc_parser = Arc::new(RuntimePermissionDescriptorParser::new(
+                sys_traits::impls::RealSys,
+            ));
 
-            js_runtime.run_event_loop(Default::default()).await?;
+            let mut worker = MainWorker::bootstrap_from_options(
+                &main_module,
+                WorkerServiceOptions::<
+                    DenoInNpmPackageChecker,
+                    NpmResolver<sys_traits::impls::RealSys>,
+                    sys_traits::impls::RealSys,
+                > {
+                    module_loader: Rc::new(FsModuleLoader),
+                    permissions: PermissionsContainer::allow_all(permission_desc_parser),
+                    blob_store: Default::default(),
+                    broadcast_channel: Default::default(),
+                    feature_checker: Default::default(),
+                    node_services: Default::default(),
+                    npm_process_state_provider: Default::default(),
+                    root_cert_store_provider: Default::default(),
+                    fetch_dns_resolver: Default::default(),
+                    shared_array_buffer_store: Default::default(),
+                    compiled_wasm_module_store: Default::default(),
+                    v8_code_cache: Default::default(),
+                    fs,
+                },
+                WorkerOptions {
+                    extensions: vec![rn_wgpu::init_ops_and_esm()],
+                    ..Default::default()
+                },
+            );
 
-            result.await.map_err(|e| AnyError::from(e))
-        }) {
-            eprintln!("error: {}", error);
-        }
+            worker.js_runtime.op_state().borrow_mut().put(app_state);
+            (worker.execute_main_module(&main_module).await).unwrap();
+            (worker.run_event_loop(false).await).unwrap();
+        });
     });
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
-import { console, setTimeout, setInterval } from "./std.js";
-
-const create_rect = Deno.core.ops.op_create_rect;
-const update_rect = Deno.core.ops.op_update_rect;
-const append_rect_to_window = Deno.core.ops.op_append_rect_to_window;
-const op_remove_rect = Deno.core.ops.op_remove_rect;
-
-const id = create_rect(100, 100, 200, 200);
-append_rect_to_window(id);
+const id = RnWGPU.create_rect(100, 100, 200, 200);
+RnWGPU.append_rect_to_window(id);
 
 console.log(`Added rect with id: ${id}`);
 
 setTimeout(() => {
-  update_rect(id, 100, 100, 600, 600);
+  RnWGPU.update_rect(id, 100, 100, 600, 600);
 }, 500);
+
+console.log("Hello from main.js");
