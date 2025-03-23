c55fd0a927ccac388fd77ac2a1c1b22a743dcb8f
Lukas Bombach <lukas.bombach@googlemail.com>
2025-03-23 18:09:08 +0100

    added my extension

--- src/js_runtime.rs
+++ src/js_runtime.rs
@@ -1,3 +1,7 @@
+#![allow(clippy::print_stdout)]
+#![allow(clippy::print_stderr)]
+
+use std::collections::HashSet;
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -5,13 +9,149 @@ use std::thread;
 
 use rustyscript::{Module, Runtime, RuntimeOptions};
 
+use deno_core::extension;
+use deno_core::op2;
+use deno_core::OpState;
+use deno_core::Resource;
+use deno_error::JsErrorBox;
+
 use crate::app::AppState;
+use crate::app::Js;
+use crate::graphics::Rect;
+
+struct RectResource(Arc<Mutex<Rect>>);
+impl Resource for RectResource {}
+
+#[op2(fast)]
+fn op_create_rect(state: &mut OpState, x: u32, y: u32, w: u32, h: u32) -> Result<u32, JsErrorBox> {
+    let rect = Arc::new(Mutex::new(Rect(x, y, w, h)));
+
+    let resource_table = &mut state.resource_table;
+    let rid = resource_table.add(RectResource(rect.clone()));
+
+    Ok(rid)
+}
+
+#[op2(fast)]
+fn op_append_rect_to_window(state: &mut OpState, rid: u32) -> Result<(), JsErrorBox> {
+    let resource_table = &mut state.resource_table;
+    let rect_resource = resource_table.get::<RectResource>(rid).unwrap();
+    let rect = rect_resource.0.clone();
+
+    state
+        .borrow::<Arc<Mutex<AppState>>>()
+        .lock()
+        .unwrap()
+        .rects
+        .lock()
+        .unwrap()
+        .push(rect.clone());
+
+    state
+        .borrow::<Arc<Mutex<AppState>>>()
+        .lock()
+        .unwrap()
+        .event_loop
+        .lock()
+        .unwrap()
+        .send_event(Js::RectsUpdated)
+        .unwrap();
+
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_update_rect(
+    state: &mut OpState,
+    rid: u32,
+    x: u32,
+    y: u32,
+    w: u32,
+    h: u32,
+) -> Result<(), JsErrorBox> {
+    let resource_table = &mut state.resource_table;
+    let rect_resource = resource_table.get::<RectResource>(rid).unwrap();
+    let rect = rect_resource.0.clone();
+
+    let mut rect = rect.lock().unwrap();
+    rect.0 = x;
+    rect.1 = y;
+    rect.2 = w;
+    rect.3 = h;
+
+    state
+        .borrow::<Arc<Mutex<AppState>>>()
+        .lock()
+        .unwrap()
+        .event_loop
+        .lock()
+        .unwrap()
+        .send_event(Js::RectsUpdated)
+        .unwrap();
+
+    Ok(())
+}
+
+#[op2(fast)]
+fn op_remove_rect_from_window(state: &mut OpState, rid: u32) -> Result<(), JsErrorBox> {
+    let resource_table = &mut state.resource_table;
+    let rect_resource = resource_table.take::<RectResource>(rid).unwrap();
+    let rect = rect_resource.0.clone();
+
+    state
+        .borrow::<Arc<Mutex<AppState>>>()
+        .lock()
+        .unwrap()
+        .rects
+        .lock()
+        .unwrap()
+        .retain(|item| !Arc::ptr_eq(item, &rect));
+
+    state
+        .borrow::<Arc<Mutex<AppState>>>()
+        .lock()
+        .unwrap()
+        .event_loop
+        .lock()
+        .unwrap()
+        .send_event(Js::RectsUpdated)
+        .unwrap();
+
+    Ok(())
+}
+
+extension!(
+    rect_extension,
+    ops = [
+        op_create_rect,
+        op_append_rect_to_window,
+        op_update_rect,
+        op_remove_rect_from_window,
+    ],
+    esm_entry_point = "rn-wgpu:rect",
+    esm = [ dir "src", "rn-wgpu:rect" = "extension.js" ],
+);
 
 pub fn run_script(app_state: Arc<Mutex<AppState>>, js_path: &str) {
     let js_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(js_path);
 
+    let mut schema_whlist = HashSet::new();
+    schema_whlist.insert("rn-wgpu:".to_string());
+
     let _handle = thread::spawn(move || {
-        let mut runtime = Runtime::new(RuntimeOptions::default()).unwrap();
+        let mut runtime = Runtime::new(RuntimeOptions {
+            schema_whlist,
+            extensions: vec![rect_extension::init_ops_and_esm()],
+            ..RuntimeOptions::default()
+        })
+        .unwrap();
+
+        runtime
+            .deno_runtime()
+            .op_state()
+            .borrow_mut()
+            .put(app_state);
+
         runtime.set_current_dir("src").unwrap();
         let module = Module::load(js_path).unwrap();
         runtime.load_module(&module).unwrap();
--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 import * as emoji from "npm:node-emoji";
+import { create_rect, append_rect_to_window, update_rect } from "rn-wgpu:rect";
 
+const id = create_rect(100, 100, 200, 200);
+append_rect_to_window(id);
+
+console.log(`Added rect with id: ${id}`);
+
+setTimeout(() => {
+  update_rect(id, 100, 100, 600, 600);
+}, 500);
+
+console.log("Hello from main.js");
 console.log(emoji.emojify(`:sauropod: :heart:  npm`));
