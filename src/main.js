14c7d6bf9534620feabafce99715b91be023b067
Lukas Bombach <lukas.bombach@googlemail.com>
2025-03-23 20:54:56 +0100

    js cleanup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as emoji from "npm:node-emoji";
+import Reconciler from "npm:react-reconciler";
 import { create_rect, append_rect_to_window, update_rect } from "rn-wgpu:rect";
 
 const id = create_rect(100, 100, 200, 200);
@@ -10,5 +10,4 @@ setTimeout(() => {
   update_rect(id, 100, 100, 600, 600);
 }, 500);
 
-console.log("Hello from main.js");
-console.log(emoji.emojify(`:sauropod: :heart:  npm`));
+console.log(typeof Reconciler);
