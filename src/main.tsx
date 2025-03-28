632524463031ebec77e61cedfe49292748635ea8
Lukas Bombach <lukas.bombach@googlemail.com>
2025-03-28 22:51:16 +0100

    children, lol

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "npm:react";
 import { ReactWGPU } from "./renderer.ts";
 
 ReactWGPU.render(
-  <>
-    <rectangle top={200} left={200} width={400} height={400} />
+  <rectangle top={200} left={200} width={400} height={400}>
     <rectangle top={400} left={400} width={400} height={400} />
-  </>
+    <rectangle top={500} left={500} width={200} height={200} />
+  </rectangle>
 );
