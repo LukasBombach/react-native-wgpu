a6e540b1c096820b55e5cc15267e435b2b9f8672
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-02 23:42:31 +0200

    chilren tests

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "npm:react";
 import { ReactWGPU } from "./renderer.ts";
 
 ReactWGPU.render(
-  <rectangle style={{ width: "50%", height: "50%", gap: 0, alignItems: "center", justifyContent: "space-around" }}>
-    <rectangle style={{ width: 100, height: 100 }} />
-    <rectangle style={{ width: 100, height: 100 }} />
-    <rectangle style={{ width: 100, height: 100 }} />
-    <rectangle style={{ width: 100, height: 100 }} />
+  <rectangle style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "space-around" }}>
+    <rectangle style={{ width: "50%", height: "50%", alignItems: "center", justifyContent: "space-around" }}>
+      <rectangle style={{ width: "20%", height: "20%" }} />
+      <rectangle style={{ width: "20%", height: "20%" }} />
+      <rectangle style={{ width: "20%", height: "20%" }} />
+    </rectangle>
   </rectangle>
 );
 
