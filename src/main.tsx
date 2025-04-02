8cb48407a7188ca5698ec2293e2994c29bed84bf
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-02 15:54:16 +0200

    x

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from "npm:react";
 import { ReactWGPU } from "./renderer.ts";
 
-ReactWGPU.render(
-  <>
-    <rectangle style={{ width: "100px", height: "auto" }} />
-  </>
-);
+ReactWGPU.render(<rectangle style={{ width: "100%", height: "100%" }} />);
 
 /* setTimeout(() => {
   console.log("");
