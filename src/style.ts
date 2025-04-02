385dbcfdc95ae994963460ce33ecd22683fdf4f3
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-02 22:42:35 +0200

    wip

--- src/style.ts
+++ src/style.ts
@@ -1,71 +1,11 @@
 import type { CSSProperties } from "react";
 
-export type TaffyDefaults = typeof defaults;
-
-export const defaults = {
-  display: "Flex",
-  item_is_table: false,
-  box_sizing: "BorderBox",
-  overflow: { x: "Visible", y: "Visible" },
-  scrollbar_width: 0,
-  position: "Relative",
-  inset: {
-    left: "Auto",
-    right: "Auto",
-    top: "Auto",
-    bottom: "Auto",
-  },
-  size: { width: "Auto", height: "Auto" },
-  min_size: { width: "Auto", height: "Auto" },
-  max_size: { width: "Auto", height: "Auto" },
-  aspect_ratio: null,
-  margin: {
-    left: { Length: 0 },
-    right: { Length: 0 },
-    top: { Length: 0 },
-    bottom: { Length: 0 },
-  },
-  padding: {
-    left: { Length: 0 },
-    right: { Length: 0 },
-    top: { Length: 0 },
-    bottom: { Length: 0 },
-  },
-  border: {
-    left: { Length: 0 },
-    right: { Length: 0 },
-    top: { Length: 0 },
-    bottom: { Length: 0 },
-  },
-  align_items: null,
-  align_self: null,
-  justify_items: null,
-  justify_self: null,
-  align_content: null,
-  justify_content: null,
-  gap: {
-    width: { Length: 0 },
-    height: { Length: 0 },
-  },
-  text_align: "Auto",
-  flex_direction: "Row",
-  flex_wrap: "NoWrap",
-  flex_basis: "Auto",
-  flex_grow: 0,
-  flex_shrink: 1,
-  grid_template_rows: [],
-  grid_template_columns: [],
-  grid_auto_rows: [],
-  grid_auto_columns: [],
-  grid_auto_flow: "Row",
-  grid_row: { start: "Auto", end: "Auto" },
-  grid_column: { start: "Auto", end: "Auto" },
-} as const;
-
-export interface Style {
+interface Style {
   size?: Size<LengthPercentage>;
   gap?: Size<LengthPercentage>;
 }
+
+type AlignItems = "Start" | "End" | "FlexStart" | "FlexEnd" | "Center" | "Baseline" | "Stretch";
 
 interface Size<T> {
   width: T;
@@ -131,3 +71,63 @@ function length(value: string | number): LengthPercentage {
   if (value === "auto") return "Auto";
   throw new Error(`Invalid CSS length: ${value} (${typeof value})`);
 }
+
+const defaults = {
+  display: "Flex",
+  item_is_table: false,
+  box_sizing: "BorderBox",
+  overflow: { x: "Visible", y: "Visible" },
+  scrollbar_width: 0,
+  position: "Relative",
+  inset: {
+    left: "Auto",
+    right: "Auto",
+    top: "Auto",
+    bottom: "Auto",
+  },
+  size: { width: "Auto", height: "Auto" },
+  min_size: { width: "Auto", height: "Auto" },
+  max_size: { width: "Auto", height: "Auto" },
+  aspect_ratio: null,
+  margin: {
+    left: { Length: 0 },
+    right: { Length: 0 },
+    top: { Length: 0 },
+    bottom: { Length: 0 },
+  },
+  padding: {
+    left: { Length: 0 },
+    right: { Length: 0 },
+    top: { Length: 0 },
+    bottom: { Length: 0 },
+  },
+  border: {
+    left: { Length: 0 },
+    right: { Length: 0 },
+    top: { Length: 0 },
+    bottom: { Length: 0 },
+  },
+  align_items: null,
+  align_self: null,
+  justify_items: null,
+  justify_self: null,
+  align_content: null,
+  justify_content: null,
+  gap: {
+    width: { Length: 0 },
+    height: { Length: 0 },
+  },
+  text_align: "Auto",
+  flex_direction: "Row",
+  flex_wrap: "NoWrap",
+  flex_basis: "Auto",
+  flex_grow: 0,
+  flex_shrink: 1,
+  grid_template_rows: [],
+  grid_template_columns: [],
+  grid_auto_rows: [],
+  grid_auto_columns: [],
+  grid_auto_flow: "Row",
+  grid_row: { start: "Auto", end: "Auto" },
+  grid_column: { start: "Auto", end: "Auto" },
+} as const;
