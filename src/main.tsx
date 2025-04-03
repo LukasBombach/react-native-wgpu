52a4b16eb2ad52b4105ad90cd854e196fcadbace
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-03 23:25:35 +0200

    x

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,8 @@
 import React from "npm:react";
-import { debug } from "rn-wgpu:rect";
 import { ReactWGPU } from "./renderer.ts";
 
 ReactWGPU.render(
-  <rectangle style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "space-around" }}>
-    <rectangle style={{ width: 500, height: 500, alignItems: "center", gap: 50 }}>
-      <rectangle style={{ width: "20%", height: "20%" }} />
-      <rectangle style={{ width: "20%", height: "20%" }} />
-      <rectangle style={{ width: "20%", height: "20%" }} />
-    </rectangle>
+  <rectangle style={{ width: 500, height: 500, alignItems: "center", justifyContent: "space-around" }}>
+    <rectangle style={{ width: "50%", height: "50%" }}></rectangle>
   </rectangle>
 );
-
-setTimeout(() => {
-  console.log("");
-  debug();
-}, 100);
