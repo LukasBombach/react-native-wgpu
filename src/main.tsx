2ad69a30a09fe2699918f97ee4814368843b5b2c
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-04 10:41:38 +0200

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "npm:react";
 import { ReactWGPU } from "./renderer.ts";
 
 ReactWGPU.render(
-  <rectangle style={{ width: 500, height: 500, alignItems: "center", justifyContent: "space-around" }}>
-    <rectangle style={{ width: "50%", height: "50%" }}></rectangle>
+  <rectangle style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "space-around" }}>
+    <rectangle style={{ width: "500px", height: "500px" }}></rectangle>
+    <rectangle style={{ width: "500px", height: "500px" }}></rectangle>
   </rectangle>
 );
