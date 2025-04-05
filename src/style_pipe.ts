c41a6c1631dc3d6c538f26291cdcae57a8cb6af3
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-05 21:50:38 +0200

    wip

--- src/style_pipe.ts
+++ src/style_pipe.ts
@@ -26,19 +26,20 @@ type Percentage = {
 };
 
 type Auto = "Auto";
-type LengthPercentageAuto = Length | Percentage | Auto;
+type LPA = Length | Percentage | Auto;
 type PropTouple<V = string> = [key: string, value: V];
 
 function toTaffy(css: Record<string, string>) {
   return pipe(
     css,
     R.toEntries,
-    A.map(([key, value]): [string, string | Point<string> | Rect<LengthPercentageAuto>] => {
+    A.map(([key, value]): PropTouple<string> | PropTouple<Point<string>> => {
       return match([key, value])
         .with(["display", P.string], toEnum)
         .with(["boxSizing", P.string], toEnum)
+        .with(["overflow", P.string], t => pipe(t, shorthand2, point))
         .with(["position", P.string], toEnum)
-        .with(["overflow", P.string], toOverflow)
+        .with(["inset", P.string], t => pipe(t, shorthand4, rect))
         .run();
     }),
     R.fromEntries
@@ -49,13 +50,57 @@ function toEnum([key, value]: PropTouple): PropTouple {
   return [pascalCase(key), pascalCase(value)];
 }
 
-function toPoint([key, value]: PropTouple): PropTouple<Point<string>> {
-  return [pascalCase(key), { x: pascalCase(value), y: pascalCase(value) }];
+function point([key, [x, y]]: PropTouple): PropTouple<Point<string>> {
+  return [pascalCase(key), { x: pascalCase(x), y: pascalCase(y) }];
 }
 
-function toOverflow([key, value]: PropTouple<string>): PropTouple<Point<string>> {
-  const [x, y] = value.split(" ");
-  return [pascalCase(key), { x: pascalCase(x), y: pascalCase(y || x) }];
+function lpa(value: string): LPA {
+  return match(value)
+    .with(P.number, n => ({ Length: n }))
+    .with(P.string, s => {
+      if (s.endsWith("%")) {
+        return { Percentage: parseFloat(s) };
+      }
+      if (s === "auto") {
+        return "Auto";
+      }
+      return { Length: parseFloat(s) };
+    })
+    .run();
+}
+
+function rect([key, [top, right, bottom, left]]: PropTouple): PropTouple<Rect<LPA>> {
+  return [
+    pascalCase(key),
+    {
+      top: lpa(top),
+      right: lpa(right),
+      bottom: lpa(bottom),
+      left: lpa(left),
+    },
+  ];
+}
+
+function shorthand2([key, value]: PropTouple): PropTouple<string[]> {
+  const [a, b] = value.split(" ");
+  return [key, [a, b || a]];
+}
+
+function shorthand4([key, value]: PropTouple): PropTouple<[string, string, string, string]> {
+  const values = value.split(" ");
+  const [a, b, c, d] = values;
+  switch (values.length) {
+    case 1:
+      return [key, [a, a, a, a]];
+    case 2:
+      return [key, [a, b, a, b]];
+    case 3:
+      return [key, [a, b, c, b]];
+    case 4:
+      return [key, [a, b, c, d]];
+    default:
+      throw new Error("Invalid number of values for shorthand");
+  }
 }
 
 if (import.meta.vitest) {
@@ -86,5 +131,33 @@ if (import.meta.vitest) {
 
     expect(toTaffy({ overflow: "visible scroll" })).toEqual({ Overflow: { x: "Visible", y: "Scroll" } });
     expect(toTaffy({ overflow: "clip hidden" })).toEqual({ Overflow: { x: "Clip", y: "Hidden" } });
+  });
+
+  test("inset", () => {
+    expect(toTaffy({ inset: "0" })).toEqual({
+      Inset: { top: { Length: 0 }, right: { Length: 0 }, bottom: { Length: 0 }, left: { Length: 0 } },
+    });
+    expect(toTaffy({ inset: "10px" })).toEqual({
+      Inset: { top: { Length: 10 }, right: { Length: 10 }, bottom: { Length: 10 }, left: { Length: 10 } },
+    });
+    expect(toTaffy({ inset: "10px 20px" })).toEqual({
+      Inset: { top: { Length: 10 }, right: { Length: 20 }, bottom: { Length: 10 }, left: { Length: 20 } },
+    });
+    expect(toTaffy({ inset: "10px 20px 30px" })).toEqual({
+      Inset: {
+        top: { Length: 10 },
+        right: { Length: 20 },
+        bottom: { Length: 30 },
+        left: { Length: 20 },
+      },
+    });
+    expect(toTaffy({ inset: "10px 20px 30px 40px" })).toEqual({
+      Inset: {
+        top: { Length: 10 },
+        right: { Length: 20 },
+        bottom: { Length: 30 },
+        left: { Length: 40 },
+      },
+    });
   });
 }
