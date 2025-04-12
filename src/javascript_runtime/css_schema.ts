2a3ae301d4bdc74b85ac916552d8ec6c4fa51722
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-12 14:57:27 +0200

    wip

--- src/javascript_runtime/css_schema.ts
+++ src/javascript_runtime/css_schema.ts
@@ -1,3 +1,5 @@
 import * as z from "zod";
 
 const display = z.literal(["block", "flex", "grid", "none"]);
+const boxSizing = z.literal(["border-box", "content-box"]);
+const overflow = z.literal(["visible", "hidden", "clip", "scroll", "auto"]);
