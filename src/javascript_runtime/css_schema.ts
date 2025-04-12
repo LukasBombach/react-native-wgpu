484bd9d2ecf0b364c7e924a9cbc28af8e0b44aee
Lukas Bombach <lukas.bombach@googlemail.com>
2025-04-12 13:37:51 +0200

    schema init

---
+++ src/javascript_runtime/css_schema.ts
@@ -0,0 +1,3 @@
+import * as z from "zod";
+
+const display = z.literal(["block", "flex", "grid", "none"]);
--- src/javascript_runtime/taffy.ts
+++ src/javascript_runtime/taffy.ts
@@ -2,16 +2,22 @@ import { pipe } from "fp-ts/lib/function";
 import * as R from "fp-ts/lib/Record";
 import * as A from "fp-ts/lib/Array";
 import { match, P } from "ts-pattern";
-import * as toCase from "change-case";
+import * as z from "zod";
+import { snakeCase, pascalCase } from "change-case";
 
 import type { CSSProperties } from "react";
 import type * as Taffy from "./taffy_types";
 
-export function cssToTaffy(css: CSSProperties): Partial<Taffy.Style> {
+export function cssToTaffy<T extends CSSProperties>(css: T): Partial<Taffy.Style> {
   const taffy: Partial<Taffy.Style> = {};
+  const keys = Object.keys(css) as (keyof T)[];
 
-  for (let k in css) {
-    console.log(k, css[k]); // This works
+  for (const k of keys) {
+    match(k)
+      .with("display", () => {
+        taffy["display"] = pascalCase(css[k]);
+      })
+      .otherwise(() => console.warn(`Unknown CSS property "${k}: ${css[k]}"`));
   }
 
   return taffy;
