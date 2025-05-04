bc5ec573f955df2f2024f4ec14e180e4b10fc204
Lukas Bombach <lukas.bombach@googlemail.com>
2025-05-04 16:28:52 +0200

    (feat) div instead of rectangle

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { ReactWGPU } from "./javascript_runtime/react_wgpu.ts";
 
 ReactWGPU.render(
-  <rectangle style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "center", gap: "10%" }}>
-    <rectangle style={{ width: "500px", height: "500px" }}></rectangle>
-    <rectangle style={{ width: "500px", height: "500px" }}></rectangle>
-  </rectangle>
+  <div style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "center", gap: "10%" }}>
+    <div style={{ width: "500px", height: "500px" }}></div>
+    <div style={{ width: "500px", height: "500px" }}></div>
+  </div>
 );
 
 // import { get_style_defaults } from "rn-wgpu:rect";
