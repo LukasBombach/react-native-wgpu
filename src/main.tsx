8b50113ea64f1ae69677f256687d2944793926ed
Lukas Bombach <lukas.bombach@googlemail.com>
2025-05-25 22:22:23 +0200

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,8 @@ import React from "react";
 import { ReactWGPU } from "./javascript_runtime/react_wgpu.ts";
 
 ReactWGPU.render(
-  <div
-    style={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      gap: 100,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "#fff",
-    }}
-  >
-    <div style={{ width: 500, height: 500, backgroundColor: "#f00" }}></div>
-    <div style={{ width: 500, height: 500, backgroundColor: "#00f" }}></div>
+  <div style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "center", gap: "10%" }}>
+    <div style={{ width: "500px", height: "500px" }}></div>
+    <div style={{ width: "500px", height: "500px" }}></div>
   </div>
 );
