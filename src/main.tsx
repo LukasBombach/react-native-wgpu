82376c97445b6f6ac55a685346a08ae9586e0572
Lukas Bombach <lukas.bombach@googlemail.com>
2025-05-30 12:17:06 +0200

    Update to the main demo

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,17 @@ ReactWGPU.render(
     style={{
       width: "100%",
       height: "100%",
-      alignItems: "center",
-      justifyContent: "center",
-      padding: "10%",
-      gap: "10%",
+      alignItems: "start",
+      padding: "100px",
     }}
   >
-    <div style={{ width: "500px", height: "500px", backgroundColor: "#f00", borderRadius: 20 }}></div>
-    <div style={{ width: "500px", height: "500px", backgroundColor: "#0f0", borderRadius: 5 }}></div>
-    <div style={{ width: "500px", height: "500px", backgroundColor: "#00f", borderRadius: 999 }}></div>
+    <div
+      style={{
+        width: "100%",
+        aspectRatio: "16/9",
+        backgroundColor: "#fff",
+        borderRadius: 10,
+      }}
+    ></div>
   </div>
 );
