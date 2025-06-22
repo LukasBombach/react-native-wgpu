98120ec791d937b755ec6d8607933d2bc38f4a6b
Lukas Bombach <lukas.bombach@googlemail.com>
2025-06-22 21:50:08 +0200

    compute_text_layout

--- src/gui/text.rs
+++ src/gui/text.rs
@@ -1,5 +1,5 @@
 use cosmic_text::{Buffer, FontSystem, Metrics, SwashCache};
-use taffy::{AvailableSpace, LayoutInput, Size};
+use taffy::{AvailableSpace, LayoutInput, LayoutOutput, Size};
 
 pub struct TextRenderer {
     pub font_system: FontSystem,
@@ -7,14 +7,19 @@ pub struct TextRenderer {
 }
 
 impl TextRenderer {
-    pub fn compute_text_layout(&mut self, text: &str, metrics: Metrics, inputs: LayoutInput) {
+    pub fn compute_text_layout(
+        &mut self,
+        text: &str,
+        metrics: Metrics,
+        inputs: LayoutInput,
+    ) -> LayoutOutput {
         // A Buffer provides shaping and layout for a UTF-8 string, create one per text widget
         let mut buffer = Buffer::new(&mut self.font_system, metrics);
 
         // Borrow buffer together with the font system for more convenient method calls
         let mut buffer = buffer.borrow_with(&mut self.font_system);
 
-        // determine the width constraint
+        // determine the width the text has to fit into
         let available_space = inputs.available_space;
         let known_dimensions = inputs.known_dimensions;
 
@@ -26,6 +31,18 @@ impl TextRenderer {
 
         buffer.set_size(width_constraint, None);
 
+        // Perform shaping as desired
         buffer.shape_until_scroll(true);
+
+        // Determine measured width and height of text
+        let (width, total_lines) = buffer
+            .layout_runs()
+            .fold((0.0, 0usize), |(width, total_lines), run| {
+                (run.line_w.max(width), total_lines + 1)
+            });
+
+        let height = total_lines as f32 * buffer.metrics().line_height;
+
+        return LayoutOutput::from_outer_size(Size { width, height });
     }
 }
